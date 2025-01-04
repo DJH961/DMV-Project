675fd5df8427ac3ddf2f014057c7a0cc652545ef
DJH961 <daniel.henke@tum.de>
2025-01-04 17:28:45 +0100

    Major update

--- Reset.sql
+++ Reset.sql
@@ -1,9 +1,3 @@
--- Reset the tables by dropping them if they exist
-DROP TABLE IF EXISTS fact_bookings;
-DROP TABLE IF EXISTS fact_success;
-DROP TABLE IF EXISTS dim_hosts;
-DROP TABLE IF EXISTS dim_listings;
-DROP TABLE IF EXISTS dim_reviews;
-DROP TABLE IF EXISTS staging_calender;
-DROP TABLE IF EXISTS staging_listings;
-DROP TABLE IF EXISTS staging_reviews;
+version https://git-lfs.github.com/spec/v1
+oid sha256:1ecb86161861a3031063a26d901152428355f984ce2b320aa46ad28cf42cd766
+size 348
